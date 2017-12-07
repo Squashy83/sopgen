a4297900bf26dda190901f27f061085397ee580a
Simone DM <simone.dellamarca@mobilesoft.it>
2017-12-07 06:41:27 +0100

    no message

--- src/app/sop-steps/sop-steps.component.ts
+++ src/app/sop-steps/sop-steps.component.ts
@@ -8,22 +8,67 @@ import { Router } from '@angular/router';
   styleUrls: ['./sop-steps.component.css']
 })
 export class SopStepsComponent implements OnInit {
-  rForm: FormGroup;
-  steps: any
+  stepsForm: FormGroup;
+  steps: any;
+  formErrors = {
+    'title': '',
+    'description': ''
+  };
+
+  validationMessages = {
+    'title': {},
+    'description': {}
+  };
+
   constructor(private fb: FormBuilder, private router: Router) {
 
   }
 
   ngOnInit() {
     this.steps = [{ 'title': 'Hi I\'m step 1', 'description': 'this is a description of a step 1!!' }, { 'title': 'Hi I\'m step 2', 'description': 'this is a description of a step 2!!' }, { 'title': 'Hi I\'m step 3', 'description': 'this is a description of a step 3!!' }];
-    this.rForm = this.fb.group({
+
+    this.setupValidationMessages();
+    this.stepsForm = this.fb.group({
       'title': [null, Validators.required],
       'description': ['', [Validators.required, Validators.minLength(20)]],
       'validate': ''
     });
+
+    this.stepsForm.valueChanges.subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.stepsForm) { return; }
+
+    const form = this.stepsForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const chiave in control.errors) {
+            if (control.errors.hasOwnProperty(chiave)) {
+              this.formErrors[field] += messages[chiave] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  setupValidationMessages() {
+    this.validationMessages.title = 'Please insert a title';
+    this.validationMessages.description = 'Please insert a description';
+
   }
 
   onNextResponsibles() {
     this.router.navigate(['/sop-responsibles']);
+  }
+
+  onBackInfo() {
+    this.router.navigate(['/sop-info']);
   }
 }
