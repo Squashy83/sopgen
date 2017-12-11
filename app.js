5a9dcd28cf729e6062b68fa67ea5d62f2ba7dd68
Simone DM <simone.dellamarca@mobilesoft.it>
2017-12-11 11:02:34 +0100

    no message

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://localhost/sopgen', {
     useMongoClient: true,
     promiseLibrary: require('bluebird')
   })
-  .then(() => console.log('connection successful'))
+  .then(() => console.log('MongoDB connection successful'))
   .catch((err) => console.error(err));
 
 app.use(logger('dev'));
@@ -29,13 +29,21 @@ app.use(logger('dev'));
 
 var rawBodySaver = function (req, res, buf, encoding) {
   if (buf && buf.length) {
-      req.rawBody = buf.toString(encoding || 'utf8');
+    req.rawBody = buf.toString(encoding || 'utf8');
   }
 };
 
-app.use(bodyParser.json({ verify: rawBodySaver}));
-app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
-app.use(bodyParser.raw({ verify: rawBodySaver, type: 'application/json'}));
+app.use(bodyParser.json({
+  verify: rawBodySaver
+}));
+app.use(bodyParser.urlencoded({
+  verify: rawBodySaver,
+  extended: true
+}));
+app.use(bodyParser.raw({
+  verify: rawBodySaver,
+  type: 'application/json'
+}));
 
 
 app.use(express.static(path.join(__dirname, 'dist')));
