943198b21adf097bf3e45a1d1140616472a79656
Simone DM <simone.dellamarca@mobilesoft.it>
2017-12-11 17:35:25 +0100

    OK

--- src/app/sop-steps/sop-steps.component.ts
+++ src/app/sop-steps/sop-steps.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PdfManagerService } from '../_services/pdf-manager.service';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-sop-steps',
@@ -22,16 +24,18 @@ export class SopStepsComponent implements OnInit {
   };
 
   constructor(private fb: FormBuilder,
-              private router: Router,
-              private pdfManager: PdfManagerService) {
-
+    private router: Router,
+    private translate: TranslateService,
+    private pdfManager: PdfManagerService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
   }
 
   ngOnInit() {
     this.steps = [{ 'title': '', 'description': '' }];
     this.setupValidationMessages();
     this.stepsForm = this.fb.group({
-      'title': [null, Validators.required],
+      'title': ['', [Validators.required]],
       'description': ['', [Validators.required, Validators.minLength(20)]],
       'validate': ''
     });
@@ -42,6 +46,7 @@ export class SopStepsComponent implements OnInit {
   }
 
   onValueChanged(data?: any) {
+    // console.log('ECCCO I DATI IN INPUT: ' + JSON.stringify(data));
     if (!this.stepsForm) { return; }
 
     const form = this.stepsForm;
@@ -63,13 +68,56 @@ export class SopStepsComponent implements OnInit {
   }
 
   setupValidationMessages() {
-    this.validationMessages.title = 'Please insert a title';
-    this.validationMessages.description = 'Please insert a description';
+    this.translate.get('VALIDATION_MESSAGES').subscribe((mes: string) => {
+      this.validationMessages.title['required'] = mes['TITLE']['REQUIRED'];
+      this.validationMessages.description['required'] = mes['DESCRIPTION']['REQUIRED'];
+    });
+  }
 
+  onCheckForm(): string {
+    let fieldMessageErrors = null;
+    if (!this.stepsForm) { return; }
+
+    const form = this.stepsForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+        if (control && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const chiave in control.errors) {
+            if (control.errors.hasOwnProperty(chiave)) {
+              this.formErrors[field] += messages[chiave] + ' ';
+            }
+          }
+          // ASSIGN FIRST ERROR
+          if (!fieldMessageErrors) {
+            fieldMessageErrors = this.formErrors[field];
+          }
+        }
+      }
+    }
+    return fieldMessageErrors;
+  }
+
+  saveGeneralInfo() {
+
+    if (this.stepsForm.valid) {
+
+      const dataToSave = this.stepsForm.value;
+
+      this.pdfManager.pdfStructure.steps = this.steps;
+
+      this.router.navigate(['/sop-responsibles']);
+
+    } else {
+      this.onCheckForm();
+    }
   }
 
   onNextResponsibles() {
-    this.router.navigate(['/sop-responsibles']);
+    this.saveGeneralInfo();
   }
 
   onBackInfo() {
