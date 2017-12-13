a26572a53f18548fda4762f3220e8c619b27eabb
Simone DM <simone.dellamarca@mobilesoft.it>
2017-12-14 00:31:50 +0100

    Il BACK ANCORA NON C'E' lo sistemo domani

--- src/app/sop-responsibles/sop-responsibles.component.ts
+++ src/app/sop-responsibles/sop-responsibles.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 import { PdfManagerService } from './../_services/pdf-manager.service';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -13,11 +13,26 @@ export class SopResponsiblesComponent implements OnInit {
   respsForm: FormGroup;
   resps: any;
   concs: any;
+  data2Save: any;
   formErrors = {
     'name': '',
     'position': '',
     'tel_code': '',
     'email_code': ''
+  };
+
+  formErrorsH = {
+    'name': '',
+    'position': '',
+    'tel_code': '',
+    'email_code': ''
+  };
+
+  formErrorsH2 = {
+    'nameC': '',
+    'positionC': '',
+    'tel_codeC': '',
+    'email_codeC': ''
   };
 
   validationMessages = {
@@ -35,40 +50,93 @@ export class SopResponsiblesComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    //RESPONSIBLES
-    this.resps = [{ 'name': '', 'position': '', 'tel_code': '', 'email_code': '' }];
-
-    //CONCERNEDs
-    this.concs = [{ 'name': '', 'position': '', 'tel_code': '', 'email_code': '' }];
-
     this.setupValidationMessages();
 
     this.respsForm = this.fb.group({
-      'name': [null, Validators.required],
-      'position': [null, Validators.required],
-      'tel_code': [''],
-      'email_code': ['']
+      resps: this.fb.array([this.initRespsConcs(undefined)]),
+      concs: this.fb.array([this.initRespsConcs('concs')]),
     });
 
     this.respsForm.valueChanges.subscribe(data => this.onValueChanged(data));
 
   }
 
+  initRespsConcs(data: any) {
+    // var resulInit = [{}];
+    // if (data) {
+    //   //CONCS
+    //   if (this.pdfManager.pdfStructure.concernPersons) {
+    //     console.log(this.pdfManager.pdfStructure.concernPersons);
+    //     for (var i = 0; i < (this.pdfManager.pdfStructure.concernPersons).length; i++) {
+    //       resulInit.push(
+    //         {
+    //           'name': [this.pdfManager.pdfStructure.concernPersons[i].name, Validators.required],
+    //           'position': [this.pdfManager.pdfStructure.concernPersons[i].position, Validators.required],
+    //           'tel_code': [this.pdfManager.pdfStructure.concernPersons[i].tel_code],
+    //           'email_code': [this.pdfManager.pdfStructure.concernPersons[i].email_code]
+    //         }
+    //       );
+    //     }
+    //     return this.fb.group(resulInit);
+    //   }
+    // } else {
+    //   //RESPS
+    //   if (this.pdfManager.pdfStructure.responsibles) {
+    //     for (var i = 0; i < (this.pdfManager.pdfStructure.responsibles).length; i++) {
+    //       resulInit.push(
+    //         {
+    //           'name': [this.pdfManager.pdfStructure.responsibles[i].name, Validators.required],
+    //           'position': [this.pdfManager.pdfStructure.responsibles[i].position, Validators.required],
+    //           'tel_code': [this.pdfManager.pdfStructure.responsibles[i].tel_code],
+    //           'email_code': [this.pdfManager.pdfStructure.responsibles[i].email_code]
+    //         }
+    //       );
+    //     }
+    //     return this.fb.group(resulInit);
+    //   }
+    return this.fb.group({
+      'name': [null, Validators.required],
+      'position': [null, Validators.required],
+      'tel_code': [''],
+      'email_code': ['']
+    });
+    // }
+
+
+  }
+
   onValueChanged(data?: any) {
     if (!this.respsForm) { return; }
-
-    const form = this.respsForm;
-    for (const field in this.formErrors) {
+    this.data2Save = data;
+    for (const field in this.formErrorsH) {
       if (this.formErrors.hasOwnProperty(field)) {
-        this.formErrors[field] = '';
-        const control = form.get(field);
+        for (var i = 0; i < ((<FormArray>this.respsForm.get('resps')).controls).length; i++) {
+          this.formErrors[field + i] = '';
+          var control = (<FormGroup>((<FormArray>this.respsForm.get('resps')).controls)[i]).controls[field];
+          if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const chiave in control.errors) {
+              if (control.errors.hasOwnProperty(chiave)) {
+                this.formErrors[field + i] += messages[chiave] + ' ';
+              }
+            }
+          }
+        }
+      }
+    }
 
-        if (control && control.dirty && !control.valid) {
-          const messages = this.validationMessages[field];
-          for (const chiave in control.errors) {
-            if (control.errors.hasOwnProperty(chiave)) {
-              this.formErrors[field] += messages[chiave] + ' ';
+
+    for (const field in this.formErrorsH) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        for (var i = 0; i < ((<FormArray>this.respsForm.get('concs')).controls).length; i++) {
+          this.formErrors[field + 'C' + i] = '';
+          var control = (<FormGroup>((<FormArray>this.respsForm.get('concs')).controls)[i]).controls[field];
+          if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const chiave in control.errors) {
+              if (control.errors.hasOwnProperty(chiave)) {
+                this.formErrors[field + 'C' + i] += messages[chiave] + ' ';
+              }
             }
           }
         }
@@ -76,7 +144,14 @@ export class SopResponsiblesComponent implements OnInit {
     }
   }
 
-
+  newRespsConcs() {
+    return this.fb.group({
+      'name': [null, Validators.required],
+      'position': [null, Validators.required],
+      'tel_code': [''],
+      'email_code': ['']
+    });
+  }
 
   setupValidationMessages() {
     this.translate.get('VALIDATION_MESSAGES').subscribe((mes: string) => {
@@ -90,31 +165,31 @@ export class SopResponsiblesComponent implements OnInit {
   }
 
   onAddResp() {
-    const dataToSave = this.respsForm.value;
-    this.resps.push({
-      'name': dataToSave.name,
-      'position': dataToSave.position,
-      'telCode': dataToSave.tel_code,
-      'emailCode': dataToSave.email_code
-    });
+    const control = <FormArray>this.respsForm.controls['resps'];
+    control.push(this.newRespsConcs());
   }
 
   onRemoveResp(index) {
-    this.resps.splice(index, 1);
+    const control = <FormArray>this.respsForm.controls['resps'];
+    control.removeAt(index);
   }
 
   onAddConc() {
-    this.concs.push({ 'title': '', 'description': '' });
+    const control = <FormArray>this.respsForm.controls['concs'];
+    control.push(this.newRespsConcs());
   }
 
   onRemoveConc(index) {
-    this.concs.splice(index, 1);
+    const control = <FormArray>this.respsForm.controls['concs'];
+    control.removeAt(index);
   }
 
   onNextFooter() {
-    this.pdfManager.pdfStructure.responsibles = this.resps;
-    this.pdfManager.pdfStructure.concernPersons = this.concs;
-    this.router.navigate(['/sop-footer']);
+    if (this.respsForm.valid) {
+      this.pdfManager.pdfStructure.responsibles = this.data2Save.resps;
+      this.pdfManager.pdfStructure.concernPersons = this.data2Save.concs;
+      this.router.navigate(['/sop-footer']);
+    }
   }
 }
 
--- src/app/sop-steps/sop-steps.component.ts
+++ src/app/sop-steps/sop-steps.component.ts
@@ -13,9 +13,8 @@ import { Location } from '@angular/common';
 })
 export class SopStepsComponent implements OnInit {
   stepsForm: FormGroup;
-
+  data2Save: any;
   steps: any;
-  // countStep = 1;
   formErrors = {
     'title': '',
     'description': ''
@@ -44,12 +43,7 @@ export class SopStepsComponent implements OnInit {
 
   ngOnInit() {
 
-    if (this.pdfManager.pdfStructure.steps) {
-      for (var step in this.pdfManager.pdfStructure.steps) {
-        this.steps.push(step);
-      }
-    } else
-      this.steps = [{ 'number': '', 'title': '', 'description': '', 'baloon': '' }];
+    this.steps = [{ 'number': '', 'title': '', 'description': '', 'baloon': '' }];
     this.setupValidationMessages();
     this.stepsForm = this.fb.group({
       steps: this.fb.array([this.initSteps()])
@@ -57,16 +51,8 @@ export class SopStepsComponent implements OnInit {
 
 
 
-    // itemRows: this.fb.array(
-    //   'title': ['', [Validators.required]],
-    //   'description': ['', [Validators.required, Validators.minLength(20)]],
-    //   'validate': ''
-    // )
-
-
     this.stepsForm.valueChanges.subscribe(data => this.onValueChanged(data));
 
-    //console.log('pdf manager structure in cache: ', this.pdfManager.pdfStructure);
   }
 
 
@@ -77,34 +63,20 @@ export class SopStepsComponent implements OnInit {
       'validate': ''
     });
   }
-  // initItemRows() {
-  //   return this._fb.group({
-  //     itemname: ['']
-  //   });
-  // }
 
   onValueChanged(data?: any) {
-    // if (!this.stepsForm) { return; }
     if (!this.stepsForm) { return; }
-
-    //const form = this.stepsForm;
+    this.data2Save = data;
     for (const field in this.formErrorsH) {
       if (this.formErrors.hasOwnProperty(field)) {
-
-
-        console.log(<FormArray>this.stepsForm.get('steps'));
         for (var i = 0; i < ((<FormArray>this.stepsForm.get('steps')).controls).length; i++) {
           this.formErrors[field + i] = '';
           var control = (<FormGroup>((<FormArray>this.stepsForm.get('steps')).controls)[i]).controls[field];
-          console.log(control);
           if (control && control.dirty && !control.valid) {
             const messages = this.validationMessages[field];
             for (const chiave in control.errors) {
-              console.log(chiave);
               if (control.errors.hasOwnProperty(chiave)) {
-
                 this.formErrors[field + i] += messages[chiave] + ' ';
-                console.log(this.formErrors[field + i]);
               }
             }
           }
@@ -121,82 +93,28 @@ export class SopStepsComponent implements OnInit {
     });
   }
 
-  onCheckForm(): string {
-    let fieldMessageErrors = null;
-    if (!this.stepsForm) { return; }
-
-    const form = this.stepsForm;
-    for (const field in this.formErrors) {
-      if (this.formErrors.hasOwnProperty(field)) {
-        this.formErrors[field] = '';
-        const control = form.get(field);
-
-        if (control && !control.valid) {
-          const messages = this.validationMessages[field];
-          for (const chiave in control.errors) {
-            if (control.errors.hasOwnProperty(chiave)) {
-              this.formErrors[field] += messages[chiave] + ' ';
-            }
-          }
-          // ASSIGN FIRST ERROR
-          if (!fieldMessageErrors) {
-            fieldMessageErrors = this.formErrors[field];
-          }
-        }
-      }
-    }
-    return fieldMessageErrors;
-  }
-
-  saveAllSteps() {
-    console.log("ECCO GLI STEP SALVATI:" + JSON.stringify(this.stepsForm.value));
-    if (this.stepsForm.valid) {
-
-      const dataToSave = this.stepsForm.value;
-
-      this.pdfManager.pdfStructure.steps = [];
-
-
-      for (var i = 0; i < (this.steps).length; i++) {
-        this.pdfManager.pdfStructure.steps[i] = this.steps[i];
-      }
-      // for (var step in this.steps) {
-      //   console.log("inserisco step: " + JSON.stringify(step));
-      //   this.pdfManager.pdfStructure.steps.push(step);
-      // }
-
-      this.router.navigate(['/sop-responsibles']);
-
-    } else {
-      this.onCheckForm();
-    }
-  }
 
   onBackInfo() {
-    // this.router.navigate(['/sop-info']);
-    this.location.back();
+    this.router.navigate(['/sop-info']);
   }
 
   onAddStep() {
     const control = <FormArray>this.stepsForm.controls['steps'];
     control.push(this.initSteps());
-    // console.log('step forms: ', this.stepsForm.value);
 
-    // const dataToSave = this.stepsForm.value;
-
-    // this.countStep++;
-
-    // this.steps.push({ 'number': '', 'title': '', 'description': '', 'baloon': '' });
   }
 
   onRemoveStep(index) {
-    // this.steps.splice(index, 1);
     const control = <FormArray>this.stepsForm.controls['steps'];
     control.removeAt(index);
-    // this.countStep--;
+
   }
 
   onNextResponsibles() {
-    this.saveAllSteps();
+    if (this.stepsForm.valid) {
+      this.pdfManager.pdfStructure.steps = this.data2Save.steps;
+      this.router.navigate(['/sop-responsibles']);
+
+    }
   }
 }
