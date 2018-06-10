f72e592bdf4c3dd7ab145290acc1b22358bef29b
Simone DM <simone.dellamarca@mobilesoft.it>
2018-06-10 18:36:49 +0200

    Please work!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ASSETS: '/assets/',
   TRANSLATE_FILE: './assets/i18n/',
-  BASE_URL: 'https://18.196.178.65/sopgen'
+  BASE_URL: 'https://18.196.178.65/sopgen/'
 };
