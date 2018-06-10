21a54a0ec3d0eea664f22731f5dbab45061a5db2
Simone DM <simone.dellamarca@mobilesoft.it>
2018-06-10 18:03:47 +0200

    Last suffix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ASSETS: '/assets/',
   TRANSLATE_FILE: './assets/i18n/',
-  BASE_URL: 'http://127.0.0.1:3000/'
+  BASE_URL: 'http://18.196.178.65/sopgen'
 };
