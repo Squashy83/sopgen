ce1e2c59fab62ab442fcf724bab0f09a97163730
Simone DM <simone.dellamarca@mobilesoft.it>
2018-06-10 17:50:58 +0200

    Base URL change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ASSETS: '/assets/',
   TRANSLATE_FILE: './assets/i18n/',
-  BASE_URL: 'http://18.195.191.72/'
+  BASE_URL: 'https://www.dr-davoli.mobi/sopgen/'
 };
